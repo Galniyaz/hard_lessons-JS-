bc531f827e93ecea772268af0fbdd16d9c8c3e26
Niyaz Gallyamov <nizik1995@yandex.ru>
2021-04-05 14:42:46 +0500

    Сделан сложный урок

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+let lang = prompt("День недели?", "")
+
+if (lang === "ru" ) {
+  alert("Пн, вт, ср, чт, пт, сб, вс");
+} else if (lang === "en" ) {
+  alert("Md, Td, Wd, Th, Fr, St, Sd");
+} else {
+  alert("Ошибка");
+}
+
+switch (lang) {
+  case "ru":
+    alert("Пн, вт, ср, чт, пт, сб, вс");
+    break;
+  case "en":
+    alert("Md, Td, Wd, Th, Fr, St, Sd");
+    break;
+}
+
+let arr = {
+	'ru':["Пн, вт, ср, чт, пт, сб, вс"],
+	'en':["Md, Td, Wd, Th, Fr, St, Sd"],
+};
+alert(arr[lang]);
+
+
+
+let namePerson = prompt('Имя?', '');
+
+let name = (namePerson === "Артем") ? console.log("директор"):
+ (namePerson === "Максим") ?  console.log("преподаватель"):
+ console.log("студент");
