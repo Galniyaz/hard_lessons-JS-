1a3308d57e1748e6b89a22b69900e8432f944e3c
Niyaz Gallyamov <nizik1995@yandex.ru>
2021-04-07 23:50:15 +0500

    Сделан сложный 5 урок

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// let arr = [ "25", "34", "41", "27", "45", "11" ];
+// arr.forEach((item) => {
+//   if (item.startsWith('2') || item.startsWith('4')) {
+//     console.log(item);
+//   }
+// });
+
+nextNum:
+for (let i = 2; i <= 100; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue nextNum;
+  }
+  console.log(i + ' Делители этого числа: 1 и ' + i);
+}
