38ccc71d875f29f84afdb6479ef3da1f7ca04fe2
Niyaz Gallyamov <nizik1995@yandex.ru>
2021-04-07 18:03:42 +0500

    Сделан 4 сложный урок

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function argum (a) {
+  if (typeof a !== 'string') {
+    alert('Не строка');
+    return;
+  }
+  a = a.trim();
+  return a.length > 30 ? a.slice(0, 30) + '...' : a;
+}
+argum();
