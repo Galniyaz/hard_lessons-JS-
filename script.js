6456c22ea23e7cecb6255a72f4217e92df8126b1
Niyaz Gallyamov <nizik1995@yandex.ru>
2021-04-12 20:32:37 +0500

    Сделан слож урок

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+let toDay = new Date().getDay() - 1;
+
+for (let i = 0; i < week.length; i++) {
+  if (i == toDay) {
+    if (week[i] == 'Суббота' || week[i] == 'Воскресенье') {
+      document.write(`<p><i>${week[i]}</i></p>`);
+    } else {
+      document.write(`<p><b>${week[i]}</b></p>`);
+    }
+  } else if (week[i] == 'Суббота' || week[i] == 'Воскресенье') {
+    document.write(`<p><i>${week[i]}<i></p>`);
+  } else {
+    document.write(`<p>${week[i]}</p>`);
+  }
+}
+console.log(week);
