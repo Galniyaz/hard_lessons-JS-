90c3b190f25521a3d18b99855b9f39c5a587c4a7
Niyaz <niyaz@example.com>
2021-11-08 18:07:09 +0500

    04Less

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const argment = function (data) {
+if (typeof data !== 'string' ) {
+    return ("Это не строка");
+} else {
+    data.trim();
+    if (data.length > 30) {
+        return ((data.substring(0,30)) + '...');
+    } else {
+        return data;
+    }
+}
+
+}
+
+console.log(argment('     Visual Studio Code - бесплатный редактор от Microsoft, в последнее  
